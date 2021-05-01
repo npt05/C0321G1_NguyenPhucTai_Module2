f4b34fc02e31075c116016f1e81cc523c0926540
TechCare <you@example.com>
2021-05-01 16:51:12 +0700

    commit

---
+++ src/bai2_vong_lap_trong_java/bai_tap/hien_thi_20_so_nguyen_to_dau_tien.java
@@ -0,0 +1,26 @@
+package bai2_vong_lap_trong_java.bai_tap;
+
+import java.util.Scanner;
+
+public class hien_thi_20_so_nguyen_to_dau_tien {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập vào số lượng số nguyên tố cần in ra: ");
+        int number = scanner.nextInt();
+        int count = 0;
+        int n = 2;
+        while (count < number) {
+            int dem = 0;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    dem++;
+                }
+            }
+            if (dem == 0) {
+                System.out.print(n + " ");
+                count++;
+            }
+            n++;
+        }
+    }
+}
