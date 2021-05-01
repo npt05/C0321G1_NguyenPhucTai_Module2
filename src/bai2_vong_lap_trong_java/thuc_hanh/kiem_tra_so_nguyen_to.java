9a1b924424cffd485d3367a40984399b5240181b
TechCare <you@example.com>
2021-05-01 09:29:12 +0700

    commit

---
+++ src/bai2_vong_lap_trong_java/thuc_hanh/kiem_tra_so_nguyen_to.java
@@ -0,0 +1,27 @@
+package bai2_vong_lap_trong_java.thuc_hanh;
+
+import java.util.Scanner;
+
+public class kiem_tra_so_nguyen_to {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a  number:");
+        int number = scanner.nextInt();
+        boolean flag = true;
+        if (number < 2) {
+            flag = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            System.out.println(number + " is a prime.");
+        }else {
+            System.out.println(number + " is not a prime.");
+        }
+    }
+}
---
+++ src/bai2_vong_lap_trong_java/thuc_hanh/ung_dung_tinh_tien_lai_cho_vay.java
@@ -0,0 +1,24 @@
+package bai2_vong_lap_trong_java.thuc_hanh;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.Scanner;
+
+public class ung_dung_tinh_tien_lai_cho_vay {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interestRate = 1.0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter investment amount: ");
+        money = scanner.nextDouble();
+        System.out.print("Enter number of month: ");
+        month = scanner.nextInt();
+        System.out.print("Enter annual interest rate in percentage: ");
+        interestRate = scanner.nextDouble();
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++) {
+            totalInterest += money * (interestRate / 100) / 12 * month;
+        }
+        System.out.println("Total of interest: " + totalInterest);
+    }
+}
