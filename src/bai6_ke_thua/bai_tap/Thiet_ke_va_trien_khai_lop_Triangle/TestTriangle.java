c7a7cb8d75ec2b685a25d27892e118b47ce4b626
TechCare <you@example.com>
2021-05-11 08:29:04 +0700

    commit

--- src/bai6_ke_thua/bai_tap/Thiet_ke_va_trien_khai_lop_Triangle/TestTriangle.java
+++ src/bai6_ke_thua/bai_tap/Thiet_ke_va_trien_khai_lop_Triangle/TestTriangle.java
@@ -1,4 +1,4 @@
-package bai6_ke_thua.bai_tap.Thiet_ke_va_trien_khai_lop_Triangle;
+package bai6_ke_thua.bai_tap.thiet_ke_va_trien_khai_lop_triangle;
 
 public class TestTriangle {
     public static void main(String[] args) {
