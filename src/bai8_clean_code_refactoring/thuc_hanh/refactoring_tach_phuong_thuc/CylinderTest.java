9832176c30748692227ccbc3025bb47e6325da46
TechCare <you@example.com>
2021-05-12 15:22:43 +0700

    commit

--- src/bai8_clean_code_refactoring/thuc_hanh/refactoring_tach_phuong_thuc/CylinderTest.java
+++
@@ -1,9 +0,0 @@
-package bai8_clean_code_refactoring.thuc_hanh.refactoring_tach_phuong_thuc;
-
-public class CylinderTest {
-    public static void main(String[] args) {
-        System.out.println("Cylinder.getVolume(12,3) = " + Cylinder.getVolume(12,3));
-        System.out.println("Cylinder.getBaseArea(12) = " + Cylinder.getBaseArea(12));
-        System.out.println("Cylinder.getPerimeter(12,3.4,Math.PI) = " + Cylinder.getPerimeter(12,3.4,Math.PI));
-    }
-}
