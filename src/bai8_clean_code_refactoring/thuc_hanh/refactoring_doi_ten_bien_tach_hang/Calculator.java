50b685260c62b0287d99b6b5126fcf39506a6ab6
TechCare <you@example.com>
2021-05-12 15:21:53 +0700

    commit

---
+++ src/bai8_clean_code_refactoring/thuc_hanh/refactoring_doi_ten_bien_tach_hang/Calculator.java
@@ -0,0 +1,27 @@
+package bai8_clean_code_refactoring.thuc_hanh.refactoring_doi_ten_bien_tach_hang;
+
+public class Calculator {
+
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+    public static int calculate(int firstOperator, int secondOperator, char operator){
+        switch (operator){
+            case ADDITION:
+                return firstOperator + secondOperator;
+            case SUBTRACTION:
+                return firstOperator - secondOperator;
+            case MULTIPLICATION:
+                return firstOperator*secondOperator;
+            case DIVISION:
+                if (secondOperator != 0)
+                    return firstOperator/secondOperator;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
---
+++ src/bai8_clean_code_refactoring/thuc_hanh/refactoring_doi_ten_bien_tach_hang/CalculatorTest.java
@@ -0,0 +1,9 @@
+package bai8_clean_code_refactoring.thuc_hanh.refactoring_doi_ten_bien_tach_hang;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int result;
+        result = Calculator.calculate(1,2,'+');
+        System.out.println("result = " + result);
+    }
+}
