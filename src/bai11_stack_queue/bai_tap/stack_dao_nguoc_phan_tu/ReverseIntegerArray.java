4ac8c3e40c56c23ace9aede8790f9039633265cd
TechCare <you@example.com>
2021-05-17 08:26:35 +0700

    commit

---
+++ src/bai11_stack_queue/bai_tap/stack_dao_nguoc_phan_tu/ReverseIntegerArray.java
@@ -0,0 +1,22 @@
+package bai11_stack_queue.bai_tap.stack_dao_nguoc_phan_tu;
+
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class ReverseIntegerArray {
+    public static void main(String[] args) {
+        int [] arrInt = new int[]{1,3,4,12,5};
+
+        Stack<Integer> stack = new Stack<>();
+        for (Integer integer : arrInt)
+            stack.push(integer);
+
+        for (int i = 0; i < arrInt.length;i ++){
+            arrInt[i] = stack.pop();
+        }
+
+        System.out.println(Arrays.toString(arrInt));
+
+    }
+}
---
+++ src/bai11_stack_queue/bai_tap/stack_dao_nguoc_phan_tu/ReverseString.java
@@ -0,0 +1,23 @@
+package bai11_stack_queue.bai_tap.stack_dao_nguoc_phan_tu;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter String :");
+
+        String string = scanner.nextLine();
+        StringBuilder outString = new StringBuilder();
+
+        String[] strArr = string.split(" ");
+        Stack<String> stack = new Stack<>();
+        for (String element : strArr)
+            stack.push(element);
+
+        while (!stack.isEmpty())
+            outString.append(stack.pop()).append(" ");
+        System.out.println(outString);
+    }
+}
