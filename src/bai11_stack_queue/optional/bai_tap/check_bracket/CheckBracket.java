fcf89ef76a2a43a8acf176c23fc1e1e902f9487a
TechCare <you@example.com>
2021-05-19 08:58:13 +0700

    commit

--- src/bai11_stack_queue/optional/bai_tap/check_bracket/CheckBracket.java
+++ src/bai11_stack_queue/optional/bai_tap/check_bracket/CheckBracket.java
@@ -4,49 +4,27 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class CheckBracket {
-    Stack<String> bStack = new Stack<>();
-    private String string;
-    private String[] stringArrays;
-
     public CheckBracket(String string) {
-        this.string = string;
-        stringToArray();
     }
 
-    public CheckBracket() {
-        setString();
-        stringToArray();
-    }
-
-    public void setString() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter String: ");
-        this.string = scanner.nextLine();
-    }
-
-    private void stringToArray() {
-        this.stringArrays = this.string.split("");
-    }
-
-    public boolean isOK() {
-        String[] strings = this.stringArrays;
-        StringBuilder left = new StringBuilder();
-        StringBuilder right = new StringBuilder();
+    public boolean chekBracket(String exp) {
+        int length = exp.length();
         Stack<String> stack = new Stack<>();
+        String[] sym = exp.split("");
 
-        for (String element : strings) {
-            if (element.equals("(")) {
-                stack.push(element);
-                left.append(element);
-            } else if (element.equals(")")) {
-                if (stack.isEmpty()) {
+        for (int i = 0; i < length; i++) {
+            if (sym[i].equals("(")) {
+                stack.push(sym[i]);
+            } else if (sym[i].equals(")")) {
+                if (stack.empty()) {
                     return false;
                 } else {
-                    right.append(element);
+                    stack.pop();
                 }
             }
         }
-        boolean isEquals = (left.length()) == (right.length());
-        return isEquals;
+        if (stack.empty()) {
+            return true;
+        } else return false;
     }
 }
--- src/bai11_stack_queue/optional/bai_tap/check_bracket/CheckBracketTest.java
+++ src/bai11_stack_queue/optional/bai_tap/check_bracket/CheckBracketTest.java
@@ -4,6 +4,6 @@ public class CheckBracketTest {
     public static void main(String[] args) {
         String string = "s * (s – a) * (s – b) * (s – c)   ";
         CheckBracket checkBracket = new CheckBracket(string);
-        System.out.println(checkBracket.isOK());
+        System.out.println(checkBracket.chekBracket(string));
     }
 }
